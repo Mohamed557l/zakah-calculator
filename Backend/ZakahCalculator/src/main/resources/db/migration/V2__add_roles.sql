f7e697fef145762bdf546efd2eae6d5b88721e62
AbdelrahmanAli1997 <abdelrahmanali1997@gmail.com>
2026-01-15 15:33:55 +0200

    v2 backend

--- Backend/ZakahCalculator/src/main/resources/db/migration/V2__add_roles.sql
+++ Backend/ZakahCalculator/src/main/resources/db/migration/V2__add_roles.sql
@@ -2,4 +2,5 @@ CREATE SEQUENCE IF NOT EXISTS role_seq_id START 1;
 INSERT INTO roles (id, type)
 VALUES
     (nextval('role_seq_id'), 'ROLE_INDIVIDUAL'),
-    (nextval('role_seq_id'), 'ROLE_COMPANY');
+    (nextval('role_seq_id'), 'ROLE_COMPANY'),
+    (nextval('role_seq_id'), 'ROLE_COMPANY_SOFTWARE');
